8438e1086f9a20572a8cff5df67f32e21f3c92e2
dylanhu7 <45575415+dylanhu7@users.noreply.github.com>
2018-12-21 23:10:04 -0500

    Fixed error which prevented image at the end from showing

--- src/Logistic+Regression+with+a+Neural+Network+mindset+v5.py
+++ src/Logistic+Regression+with+a+Neural+Network+mindset+v5.py
@@ -704,9 +704,9 @@ d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 200
 # In[231]:
 
 # Example of a picture that was wrongly classified.
-index = 1
+index = 5
 plt.imshow(test_set_x[:,index].reshape((num_px, num_px, 3)))
-print ("y = " + str(test_set_y[0,index]) + ", you predicted that it is a \"" + classes[d["Y_prediction_test"][0,index]].decode("utf-8") +  "\" picture.")
+print ("y = " + str(test_set_y[0][index]) + ", you predicted that it is a \"" + classes[int(d["Y_prediction_test"][0][index])].decode("utf-8") +  "\" picture.")
 
 
 # Let's also plot the cost function and the gradients.
