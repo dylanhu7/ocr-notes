72217f33c62d66cfe2bdc6d0549251784e8e6465
dylanhu7 <45575415+dylanhu7@users.noreply.github.com>
2018-12-29 16:42:44 -0500

    Duplicated "a" data for a more balanced dataset.

--- src/create_datasets.py
+++ src/create_datasets.py
@@ -5,7 +5,9 @@ import h5py
 import scipy
 from scipy import io
 import os
+import time
 
+%matplotlib inline
 
 # Creates an h5 file for letters called letter_train.h5 in the datasets folder by restructuring the EMNIST letter dataset, which is in Matlab format
 # Loads Matlab file
@@ -17,18 +19,22 @@ letter_train_mat = scipy.io.loadmat("datasets\emnist-letters.mat")
 
 # Creates new array and reshapes each row of the original dataset into 28x28 matrices, each representing an image
 letter_image_data = []
-for i in range(len(letter_train_mat["dataset"]["train"][0, 0]["images"][0, 0])):
-    letter_image_data.append(np.reshape(
-        letter_train_mat["dataset"]["train"][0, 0]["images"][0, 0][i], (28, 28)).T)
+# for i in range(len(letter_train_mat["dataset"]["train"][0, 0]["images"][0, 0])):
+#     letter_image_data.append(np.reshape(
+#         letter_train_mat["dataset"]["train"][0, 0]["images"][0, 0][i], (28, 28)).T)
 # Creates new array for image labels (0-25 for letters)
 letter_image_labels = []
 for i in range(len(letter_train_mat["dataset"]["train"][0, 0]["labels"][0, 0])):
     value = letter_train_mat["dataset"]["train"][0, 0]["labels"][0, 0][i][0]
+    image = np.reshape(
+        letter_train_mat["dataset"]["train"][0, 0]["images"][0, 0][i], (28, 28)).T
     if value == 1:
-        letter_image_labels.append(1)
+        for i in range(25):
+            letter_image_labels.append(1)
+            letter_image_data.append(image)
     else:
         letter_image_labels.append(0)
-
+        letter_image_data.append(image)
 # # Writes a new h5 file from the new array for compatibility with the NN
 os.remove("datasets\letter_train.h5")
 letter_train_set = h5py.File("datasets\letter_train.h5", 'w')
